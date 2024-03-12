3cc322bbdf71e1a2e4a7f5082af5b8b46632b9e4
Ollie <oliver2.forbes@live.uwe.ac.uk>
2024-03-12 15:08:12 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let mic;
+let fft;
 
  function setup(){
   let cnv = createCanvas(500, 500);
@@ -6,14 +7,86 @@ let mic;
   textAlign(CENTER);
   mic = new p5.AudioIn();
   mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
 }
 
-function draw(){
+function draw() {
   background(0);
   fill(255);
+  noStroke(); // so that the text below doesn't have a large stroke value
   text('tap to start', width/2, 20);
 
-  micLevel = mic.getLevel();
-  let y = height - micLevel * height;
-  ellipse(width/2, y, 10, 10);
+  let micLevel = mic.getLevel();
+  let mappedMicLevel = map(micLevel, 0, 0.1, 0, 200); // map micLevel to desirable range
+  let spectrum = fft.analyze();
+
+  console.log(spectrum);
+
+  let treble = fft.getEnergy("treble");
+  let mid = fft.getEnergy("mid");
+  let bass = fft.getEnergy("bass");
+
+  console.log(treble);
+  console.log(mid);
+  console.log(bass);
+
+  let mappedTreble = map(treble, 0, 50, 0, 200); 
+  let mappedMid = map(mid, 0, 255, -100, 100); 
+  let mappedBass = map(bass, 0, 255, -200, 0);
+
+  let scaleTreble = map(treble, 0, 50, 0.8, 1.2); 
+  let scaleMid = map(mid, 0, 255, -0.9, 0.9); 
+  let scaleBass = map(bass, 0, 255, -1, 1);
+
+  // treble
+  push();
+  strokeWeight(2);
+  stroke(255,0,0);
+  scale(scaleTreble);
+  point(mappedTreble, height/4);
+  pop();
+
+  // mid
+  push();
+  strokeWeight(3);
+  stroke(0,0,255);
+  scale(scaleMid);
+  point(mappedMid, height/4);
+  pop();
+
+  // bass
+  push();
+  strokeWeight(4);
+  stroke(255);
+  scale(scaleBass);
+  point(mappedBass, height/4);
+  pop();
+
+  translate(width / 2, height / 2); // move point of origin to centre of canvas
+
+  let noOfPoints = 12; // total number of points to draw
+
+  for (let i = 0; i < noOfPoints; i++) { // for all of the points
+    rotate(TWO_PI / noOfPoints); // rotate around a circle - default for p5 is radians
+
+    strokeWeight(4);
+    stroke(255);
+    point(mappedMicLevel, height/4);
+
+  // treble
+  strokeWeight(6);
+  stroke(255,0,0);
+  point(mappedTreble, height/4);
+
+  // mid
+  strokeWeight(4);
+  stroke(0,0,255);
+  point(mappedMid, height/4);
+
+  // bass
+  strokeWeight(6);
+  stroke(255);
+  point(mappedBass, height/4);
+  }
 }
