3df34ec4d54386326b270fa2978b1e7a3b27bf39
Ollie <oliver2.forbes@live.uwe.ac.uk>
2024-03-12 14:37:58 +0000

    start

---
+++ sketch.js
@@ -0,0 +1,19 @@
+let mic;
+
+ function setup(){
+  let cnv = createCanvas(500, 500);
+  cnv.mousePressed(userStartAudio);
+  textAlign(CENTER);
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw(){
+  background(0);
+  fill(255);
+  text('tap to start', width/2, 20);
+
+  micLevel = mic.getLevel();
+  let y = height - micLevel * height;
+  ellipse(width/2, y, 10, 10);
+}
